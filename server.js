49038235fb4e7e54a1f5859cecd1bee8791bf0a7
Rani Bungay <ranibungay@gmail.com>
2024-09-26 12:15:21 -0400

    connected mongodb

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import express from "express";
-
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB ", mongoose.connection.name);
+});
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");

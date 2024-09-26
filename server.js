a8e625903c60c69b1b0ba5e2299d445a76b797e0
Rani Bungay <ranibungay@gmail.com>
2024-09-26 13:58:22 -0400

    added new fruits ejs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import Fruit from "./models/fruit.js";
 dotenv.config();
 const app = express();
 
@@ -10,8 +11,14 @@ mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB ", mongoose.connection.name);
 });
 
+// render the index
 app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+//getting fruits at new
+app.get("/fruits/new", (req, res) => {
+  res.render("fruits/new.ejs");
 });
 
 app.listen(3000, () => {

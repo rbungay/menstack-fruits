97bac015e6a60cbcda74ad7068200f04431c990a
Rani Bungay <ranibungay@gmail.com>
2024-09-26 11:21:31 -0400

    installed express

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", async (req, res) => {
+  res.send("hello,friend");
+});
+
 app.listen(3000, () => {
   console.log("Listening on port 3000");
 });
